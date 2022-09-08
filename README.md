af3c7d5f16c614e63f7442ad67f8d867280c0d73
airoootiv <vpistoriguimaraes@gmail.com>
2022-09-08 11:58:19 -0300

    add-project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/airoootiv/pen/qBYdeVR](https://codepen.io/airoootiv/pen/qBYdeVR).
+
---
+++ src/script.js
@@ -0,0 +1,17 @@
+var nome = prompt("Registre seu nome.");
+
+var notaDoPrimeiroBimestre = prompt("Qual a nota do 1º bimestre?");
+var notaDoSegundoBimestre = prompt("Qual a nota do 2º bimestre?");
+var notaDoTeceiroBimestre = prompt("Qual a nota do 3º bimestre?");
+var notaDoQuartoBimestre = prompt("Qual a nota do 4º bimestre?");
+
+var notaFinal = ( parseInt(notaDoPrimeiroBimestre) + parseInt(notaDoSegundoBimestre) + parseInt(notaDoTeceiroBimestre) + parseInt(notaDoQuartoBimestre) ) / 4;
+
+var notaFixada = notaFinal.toFixed(2);
+
+alert(nome + ", sua média foi de " + notaFixada);
+
+// Isso é um comentário
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
